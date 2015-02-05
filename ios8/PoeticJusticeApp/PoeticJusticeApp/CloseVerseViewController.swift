e33a25dcf76830dfe3518d46e1ab1e6efef1f063
Mat Mathews <mat@miga.me>
2015-02-05 05:49:29 -0700

    pre-merge

--- ios8/PoeticJusticeApp/PoeticJusticeApp/CloseVerseViewController.swift
+++ ios8/PoeticJusticeApp/PoeticJusticeApp/CloseVerseViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class CloseVerseViewController: UIViewController {
 
+    @IBOutlet weak var verseTitle: UILabel!
+    @IBOutlet weak var verseText: UITextView!
+    
+    var verseId: Int = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +26,79 @@ class CloseVerseViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onClose(sender: AnyObject) {
+        println("onClose called")
+        
+        if self.verseId > 0 {
+            NetOpers.sharedInstance.get(
+                NetOpers.sharedInstance.appserver_hostname! + "/v/close/id=\(self.verseId)", {data, response, error -> Void in
+                    
+                    let httpResponse = response as NSHTTPURLResponse
+                    if httpResponse.statusCode == 200 {
+                        if data != nil {
+                            
+                            let jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(
+                                data!, options: NSJSONReadingOptions.MutableContainers,
+                                error: nil) as NSDictionary
+                            
+                            if let status = jsonResult["status"] as? String{
+                                if status == "Ok"{
+                                    
+                                }
+                            }
+                            
+                            dispatch_async(dispatch_get_main_queue(),{
+                                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                            })
+                            
+                        }
+                    }
+                    
+            })
+        }else{
+            //raise error message
+        }
+    }
+    
+    
+    func load_verse(){
+        
+        if self.verseId > 0 {
+            NetOpers.sharedInstance.get(
+                NetOpers.sharedInstance.appserver_hostname! + "/v/view/id=\(self.verseId)", {data, response, error -> Void in
+                    
+                    let httpResponse = response as NSHTTPURLResponse
+                    if httpResponse.statusCode == 200 {
+                        if data != nil {
+                            
+                            let jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(
+                                data!, options: NSJSONReadingOptions.MutableContainers,
+                                error: nil) as NSDictionary
+                            
+                            var verse = ""
+                            if let results = jsonResult["results"] as? NSDictionary{
+                                if let lines = results["lines"] as? NSArray{
+                                    for line in lines{
+                                        verse += "\(line)"
+                                    }
+                                }
+                            }
+                            
+                            dispatch_async(dispatch_get_main_queue(),{
+                                self.verseText.text = verse
+                                
+                                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                            })
+                            
+                        }
+                    }
+                    
+            })
+        }else{
+            //raise error message
+        }
+        
+    }
 
     /*
     // MARK: - Navigation
