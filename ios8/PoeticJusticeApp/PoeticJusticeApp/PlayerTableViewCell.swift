7f5419cd21848302e86fac26af21eaedbc1d76e6
Mat Mathews <mat@miga.me>
2015-02-07 09:08:47 -0700

    Avatar Icons

---
+++ ios8/PoeticJusticeApp/PoeticJusticeApp/PlayerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerTableViewCell.swift
+//  PoeticJusticeApp
+//
+//  Created by Mat Mathews on 2/6/15.
+//  Copyright (c) 2015 Miga Col.Labs. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
