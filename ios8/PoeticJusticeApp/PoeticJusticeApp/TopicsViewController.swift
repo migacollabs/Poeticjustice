e686e3f54a50ed8e8c5fa66d267717ac41f6fed7
fairchild670 <larry@miga.me>
2015-02-24 19:56:25 -0800

    Updated topic orbit to use two speeds;

--- ios8/PoeticJusticeApp/PoeticJusticeApp/TopicsViewController.swift
+++ ios8/PoeticJusticeApp/PoeticJusticeApp/TopicsViewController.swift
@@ -148,10 +148,10 @@ class ActiveTopic {
             
             println("refresh active topic_id \(activeTopicRec.topic_id) - turns left for player \(counts)")
             
-            topicStateAnimDuration = NSTimeInterval(counts) * 1.5
-            
-            if topicStateAnimDuration==0 {
+            if (counts==0) {
                 topicStateAnimDuration = 1.0
+            } else {
+                topicStateAnimDuration = 4.0
             }
             
         } else {
