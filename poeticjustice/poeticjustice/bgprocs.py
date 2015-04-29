f489c2e1c1f8d28edf73b11273dc91c3298fa843
Mat Mathews <mat@miga.me>
2015-04-29 11:57:30 -0700

    just a cleanup

--- poeticjustice/poeticjustice/bgprocs.py
+++ poeticjustice/poeticjustice/bgprocs.py
@@ -9,183 +9,126 @@ import threading
 import PIL as pil
 from boto.s3.key import Key
 from boto.s3.connection import S3Connection, Location
-from pyaella import dinj
+from pyaella import dinj, MsgChannelNames
 from pyaella.server.processes import UploadProcess
-from pyaella.tasks import TaskListProctorFactory
+from pyaella.tasks import TaskListProctorFactory, Task
 from pyaella.orm.xsqlalchemy import SQLAlchemySessionFactory
 from poeticjustice.models import *
 
 #  Boto tutorial here http://docs.pythonboto.org/en/latest/s3_tut.html
 
 __all__ = [
-    'PoeticjusticeUploadProcess',
-    'PoeticjusticeUploadThread'
+    'PoeticJusticeBkgProcessThread'
 ]
 
-class PoeticjusticeUploadProcess(UploadProcess):
-    """ """
-    AsyncFamily = 'Processing'
 
-    def setup(self):
-        self._task_list, self._task_list_lock = TaskListProctorFactory()()
-        self._sssn_fctry = SQLAlchemySessionFactory()
-        self._app_config = dinj.AppConfig()
-
-    def on_new(self, task):
-        pass
-
-class PoeticjusticeUploadThread(threading.Thread):
+class PoeticJusticeBkgProcessThread(threading.Thread):
     """ """
     AsyncFamily = 'Threading'
 
     def setup(self):
+        print 'PoeticJusticeBkgProcessThread setup called'
         self._task_list, self._task_list_lock = TaskListProctorFactory()()
         self._sssn_fctry = SQLAlchemySessionFactory()
         self._current_session = self._sssn_fctry.Session
         self._app_config = dinj.AppConfig()
+        self._dinj_config = get_dinj_config(self._app_config)
         self._dispatch = {
-            'UploadProcess_Image':self.on_new_image,
-            'UploadProcess_Video':self.on_new_video
+            'BkgProcess_CloseVerseAddToHistory':self.close_verse_add_to_history,
         }
-        if not os.path.exists(self._app_config.AssetDepot):
-            try:
-                os.makedirs(self._app_config.AssetDepot)
-            except:pass
+
+        self._autl_lut = LutValues(model=AssetUsageTypeLookup)
+
+        if not self._app_config.AssetDepot.startswith("s3://"):
+            if not os.path.exists(self._app_config.AssetDepot):
+                try:
+                    os.makedirs(self._app_config.AssetDepot)
+                except:pass
 
         self._lock = threading.RLock()
+
+        self._r_srv = None
+        self._pubsub = None
+        if('RedisServer' in self._dinj_config.Resources or 'REDISCLOUD_URL' in os.environ):
+            if 'REDISCLOUD_URL' in os.environ:
+                url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+                self._r_srv = redis.Redis(
+                    host=url.hostname, port=url.port, password=url.password)
+            elif self._dinj_config.Resources.RedisServer not in [None, '']:
+                self._r_srv = redis.Redis(self._dinj_config.Resources.RedisServer)
+            self._pubsub = self._r_srv.pubsub()
+            self._pubsub.subscribe([MsgChannelNames.TaskEvents])
+
+        self._go = threading.Event()
 
     def on_new(self, task):
         return self._dispatch[task.Target](task)
 
-    def on_new_image(self, task):
-        """
-            Pillow (PIL fork) could be used to 
-            make thumbnails easier and more
-            configurable
-        """
-        try:
-            start = datetime.datetime.now()
-            img = pil.Image.open(task.filepath)
-            w, h = img.size
-            _, ext = os.path.splitext(task.filepath)
-            
-            db_start = datetime.datetime.now()
-            
-            if self._current_session == None:
-                self._current_session = self._sssn_fctry.Session
-                
-            asset = Asset(
-                name=task.name,
-                description=task.description,
-                asset_name=task.asset_name,
-                height=h, 
-                width=w,  
-                extention=ext[1:], 
-                owner_id=task.owner_id)
 
-            # save Asset
-            asset.save(session=self._current_session)
-            asset_id = asset.id
+    def close_verse_add_to_history(self, task):
+        print task
 
-            new_path = ''
-            
-            if self._app_config.AssetDepot.startswith("s3://"):
-                bucket_name = self._app_config.AssetDepot[5:]
-                conn = S3Connection(
-                    os.environ.get('AWS_ACCESS_KEY_ID'),
-                    os.environ.get('AWS_SECRET_ACCESS_KEY')
-                )
-                asset_depot = conn.get_bucket(bucket_name)
-                asset_depot_key = Key(asset_depot)
-                new_path = task.access_token + '/' + str(asset.id)+ext
-                asset_depot_key.key = new_path
-                asset_depot_key.set_contents_from_filename(task.filepath)
-            else:
-                new_path = os.path.join(
-                    self._app_config.AssetDepot,
-                    task.access_token,
-                    str(asset.id)+ext
-                    )
-                if not os.path.exists(os.path.dirname(new_path)):
-                    try:
-                        os.makedirs(os.path.dirname(new_path))
-                    except:pass
+    def shutdown(self):
+        self._go.set()
 
-                shutil.copy2(task.filepath, new_path)
-
-            # Do something with new Asset, add to DB, etc..
-            os.remove(task.filepath)
-            
-            return True
-
-        except:
-            print traceback.format_exc()
-            try:
-                self._current_session.close()
-                time.sleep(2)
-            except:
-                pass
-            return False
-
-    def on_new_video(self, task):
-        _, ext = os.path.splitext(task.filepath)
-        sess = self._sssn_fctry.Session
-        try:
-            asset = Asset(
-                name=task.name,
-                description=task.description,
-                asset_name=task.asset_name,
-                height=0, 
-                width=0,  
-                extention=ext[1:], 
-                owner_id=task.owner_id)
-            asset.save(session=sess)
-            asset_id = asset.id
-            new_path = os.path.join(
-                self._app_config.AssetDepot,
-                task.access_token,
-                str(asset.id)+ext
-                )
-            if not os.path.exists(os.path.dirname(new_path)):
-                try:
-                    os.makedirs(os.path.dirname(new_path))
-                except:pass
-            
-            shutil.copy2(task.filepath, new_path)
-
-            # Do something with new Asset, add to DB, etc..
-            os.remove(task.filepath)
-            
-            return True
-
-        except:
-            print traceback.format_exc()
-            return False
-    
     def run(self):
+        print 'MividioBkgProcessThread.run() called'
         self.setup()
-        sleep_x = 1
+        sleep_x = .2
         to_close = set()
-        while 1:
-            try:
-                if len(to_close) > 0:
-                    try:
-                        for i in range(0,10):
-                            t = to_close.pop()
-                            with self._task_list_lock:
-                                self._task_list.ask_close(t)
-                    except:
-                        pass
-            except:
-                pass
-            time.sleep(sleep_x)
-            task = None
-            with self._task_list_lock:
-                task = self._task_list.ask_get()
-            if task:
-                sleep_x = 1
-                if self.on_new(task):
-                    to_close.add(task)
+        while not self._go.is_set():
+            if self._r_srv and self._pubsub:
+                if self._pubsub:
+                    pass
+                    #for item in self._pubsub.listen():
+                    #   print 'Message item', item
+
+                item = self._r_srv.rpoplpush(
+                    MsgChannelNames.TaskForBackgroundProcessing, 
+                    MsgChannelNames.TaskInBackgroundProcessing)
+
+                if item:
+                    with self._lock:
+                        try:
+                            self.on_new(Task ** item)
+                        except:
+                            print traceback.format_exc()
+
+                time.sleep(sleep_x)
+
             else:
-                sleep_x = 2
+                try:
+                    if len(to_close) > 0:
+                        with self._lock:
+                            try:
+                                for i in range(0,10):
+                                    t = to_close.pop()
+                                    with self._task_list_lock:
+                                        self._task_list.ask_close(t)
+                            except:
+                                pass
+                except:
+                    pass
+                
+                time.sleep(sleep_x)
+                task = None
+                with self._task_list_lock:
+                    task = self._task_list.ask_get()
+                if task:
+                    sleep_x = .1
+                    with self._lock:
+                        if self.on_new(task):
+                            to_close.add(task)
+                else:
+                    sleep_x = .5
+
+
+
+
+
+
+
+
+
+
 
